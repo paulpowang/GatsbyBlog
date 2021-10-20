b62f3a8ca1b1ccde10d211a87f9decd4a41c11df
PoHung Wang <wang.po.hung@gmail.com>
2021-10-19 19:22:44 -0700

    first entry

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,38 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <div>
+      <h1>Paul</h1>
+      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => {
+        return (
+          <div key={node.id}>
+            <span>
+              {node.frontmatter.title} - {node.frontmatter.date}
+            </span>
+            <p>{node.excerpt}</p>
+          </div>
+        )
+      })}
+    </div>
+    {
+      //   <StaticImage
+      //   src="../images/gatsby-astronaut.png"
+      //   width={300}
+      //   quality={95}
+      //   formats={["auto", "webp", "avif"]}
+      //   alt="A Gatsby astronaut"
+      //   style={{ marginBottom: `1.45rem` }}
+      // />
+    }
+
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
@@ -27,3 +41,22 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            description
+            title
+            date
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
